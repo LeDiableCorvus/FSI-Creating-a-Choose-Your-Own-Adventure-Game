9711df055a4f954294458ec0a784d16326e6e530
Static <diablecorvus@proton.me>
2022-06-27 22:22:02 -0400

    Updated .js workflow for navigation, adding new options

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 //Your Code here
 
-let usrAction = window.prompt('Travel left or right?');
-console.log(usrAction);
+let usrAction00 = window.prompt();
+if (usrAction00 == 'left'){
+    window.alert("You travelled " + usrAction00 + ". You come across a stray cat. It scampers off down a small hole just large enough for you to crawl through.");
+}
+else{
+    window.alert("You travelled " + usrAction00 + ". The universe continues into the void.");
+    window.location.reload();
+}
+if (usrAction00 == 'left'){
+    let usrAction01 = window.prompt("Do you follow the cat or continue on your path?");
+}
+
+let routeEntry = [0, 'All the universe lays before you, travel left or right?', dirL, dirR];
+
+let mapRoute = [
+    routeEntry,
+    loc01
+];
